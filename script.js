34661102ed8f2a0af6451c7783114415a929a0e7
Impanda9 <137157462+Impanda9@users.noreply.github.com>
2024-01-23 08:57:37 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+document.getElementById("quiz-form").addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const selectedAnswer = document.querySelector('button.selected');
+
+  if (selectedAnswer) {
+    const answerValue = selectedAnswer.value;
+
+    localStorage.setItem("SelectedNR", answerValue);
+    console.log("Selected answer registered in local storage:", answerValue);
+
+    window.location.href = "9.TypeTest.html";
+  } else {
+    console.log("No answer selected");
+  }
+});
+
+const answerButtons = document.querySelectorAll('.answer-option button');
+answerButtons.forEach(function (button) {
+  button.addEventListener('click', function () {
+    answerButtons.forEach(function (btn) {
+      btn.classList.remove('selected');
+    });
+
+    button.classList.add('selected');
+  });
+});
